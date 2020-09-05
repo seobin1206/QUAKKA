bdcf727234a2968f345f0a638fb84fee34b7e2ec
seobinbae836@gmail.com <seobinbae836@gmail.com>
2020-09-05 14:02:46 +0900

    add bot state

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const byeChannelComment = "안녕히가세요.";
 
 client.on('ready', () => {
   console.log('켰다.');
+  client.user.setPresence({ game: { name: '!help를 쳐보세요.' }, status: 'online' })
 });
 
 client.on("guildMemberAdd", (member) => {
