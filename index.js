d11fb68d55ab82b6baaf77769c85f0396aa80bc8
seobinbae836@gmail.com <seobinbae836@gmail.com>
2020-09-10 15:12:29 +0900

    deleted vote

--- index.js
+++ index.js
@@ -241,25 +241,5 @@ async function AutoMsgDelete(message, str, delay = 3000) {
   }, delay);
 }
 
-exports.run = async (client, message, args, tools) => {
-    
-  if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('이 명령어는 관리자 권한이 필요합니다.');
-
-  if (!args[0]) return message.channel.send('Proper Usage: <prefix>poll question');
-
-  const embed = new Discord.MessageEmbed()
-      .setColor(0xffffff)
-      .setFooter('React to vote.')
-      .setDescription(args.join(' '))
-      .setTitle(`Vote Created By ${message.author.username}`);
-
-  let msg = await message.channel.send(embed);
-
-  await msg.react('✅');
-  await msg.react('⛔');
-
-  message.delete({timeout: 10000});
-}
-
 
 client.login(token);
